38c29b25205fd59381aa9a25892d2de3f4995557
davidalejandroo28 <149003599+davidalejandroo28@users.noreply.github.com>
2024-08-04 18:14:12 -0400

    Update Graph.h

--- Color_Mapper_Program/Color_Mapper/Graph.h
+++ Color_Mapper_Program/Color_Mapper/Graph.h
@@ -1,76 +1,76 @@
-////
-//// Created by DavidD on 7/29/2024.
-////
 //
-//#ifndef PROJECT3_GRAPH_H
-//#define PROJECT3_GRAPH_H
-//#include <iostream>
-//#include <algorithm>
-//#include <vector>
-//using namespace std;
+// Created by DavidD on 7/29/2024.
 //
-//struct Shape{ //Modify structure where vector is array
-//    vector<pair<float, float>> vertices;
-//    vector<Shape*> neighbors;
-//    int color;
-//
-//    Shape(vector<pair<int, int>> v, int c){
-//        for(int i = 0; i < v.size(); i++){
-//            vertices[i] = v[i];
-//        }
-//        color = c;
-//    };
-//
-//};
-//
-//class Graph {
-//public:
-//    vector<Shape*> shapes;
-//    vector<int> colors = {1, 2, 3, 4};
-//
-//    void setNeighbors(){
-//        for(int i = 0; i < shapes.size(); i++){
-//            for(int j = 0; j < shapes.size(); j++){
-//                int common_vertices = 0;
-//                if (find(shapes[i]->vertices.begin(), shapes[i]->vertices.end(),shapes[j]->vertices[0])!= shapes[i]->vertices.end() ){
-//                    common_vertices++;
-//                }
-//                if (find(shapes[i]->vertices.begin(), shapes[i]->vertices.end(),shapes[j]->vertices[1])!= shapes[i]->vertices.end() ){
-//                    common_vertices++;
-//                }
-//                if (find(shapes[i]->vertices.begin(), shapes[i]->vertices.end(),shapes[j]->vertices[2])!= shapes[i]->vertices.end() ){
-//                    common_vertices++;
-//                }
-//                if(common_vertices == 2){
-//                    shapes[i]->neighbors.push_back(shapes[j]);
-//                }
-//                else if(common_vertices == 3){
-//                    continue;
-//                }
-//
-//            }
-//        }
-//    }
-//
-//    void coloringShapes(){
-//        for(int i = 0; i < shapes.size(); i++){
-//            colors = {1, 2, 3, 4};
-//            for(int j = 0; j < shapes[i]->neighbors.size(); j++){
-//                int temp_col = shapes[i]->neighbors[j]->color;
-//                auto it = find(colors.begin(), colors.end(), temp_col);
-//                if (it != colors.end()) {
-//                    colors.erase(it);
-//                }
-//            }
-//            if(colors.size() != 0) {
-//                shapes[i]->color = colors[0];
-//            }
-//        }
-//    }
-//
-//};
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <array>
+#include <utility>
+#include <random>
+#include <map>
+//#include "Delaunay_Triangulation.h"
+using namespace std;
+
+struct Color {
+    array<int, 3> RGB;
+    Color(int R, int G, int B){
+        RGB[0] = R;
+        RGB[1] = G;
+        RGB[2] = B;
+    }
+};
+
+class Graph {
+public:
+    void setNeighbors(vector<Triangle>& shapes) {
+        int numTriangles = shapes.size();
+        for (int i = 0; i < numTriangles; ++i) {
+            for (int j = 0; j < numTriangles; ++j) {
+                int commonVertices = 0;
+                for (int k = 0; k < 3; ++k) {
+                    if (find(begin(shapes[j].vertices), end(shapes[j].vertices), shapes[i].vertices[k]) != end(shapes[j].vertices)) {
+                        ++commonVertices;
+                    }
+                }
+                // If they share only 2 vertices, they are neighbors,
+                if (commonVertices == 2) {
+                    shapes[i].neighbors.push_back(&shapes[j]);
+                }
+            }
+        }
+    }
+
+//while(!checker)
+     void coloringShapes(Triangle& T, bool& check, Color color_par){
+
+        // check if neighbors have color_par
+        for(int i = 0; i < T.neighbors.size(); i++){
+            if(T.neighbors[i]->color == T.color){
+                return; // just return because a neighbor has the same color as Triangle parameter
+            }
+        }
+        check = true; // if neighbors don't have color check = true
+        for(int j = 0; j < 3; j++){
+            T.color[0] = color_par.RGB[0];
+            T.color[1] = color_par.RGB[1];
+            T.color[2] = color_par.RGB[2];
+        }
+    }
 
 
 
 
-//#endif //PROJECT3_GRAPH_H
+};
+
+
+
+
+
+
+
+
+
+
+
+
